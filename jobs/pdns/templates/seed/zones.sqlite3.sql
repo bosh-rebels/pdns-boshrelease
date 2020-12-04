e7cad0a78c9cc711ab243633add627502ce8ecab
Diego Fidalgo Moreno <Diego.FidalgoMoreno@santander.co.uk>
2020-12-04 18:41:07 +0000

    Adding SOA Record automatically

--- jobs/pdns/templates/seed/zones.sqlite3.sql
+++ jobs/pdns/templates/seed/zones.sqlite3.sql
@@ -1,6 +1,14 @@
 <% p('zones').each_with_index do |zone, domain_id| %>
 INSERT INTO domains (name, type) values ('<%= zone.fetch('name') %>', 'NATIVE');
-
+INSERT INTO records (domain_id, name, content, type,ttl,prio)
+VALUES (
+    <%= domain_id + 1 %>,
+    '<%= zone.fetch('name') %>',
+    'localhost admin.<%= zone.fetch('name')%> 1 10380 3600 604800 3600',
+    'SOA',
+    86400,
+    NULL
+);
   <% zone.fetch('records').each do |record| %>
 INSERT INTO records (domain_id, name, content, type,ttl,prio)
 VALUES (
